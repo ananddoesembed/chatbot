8ff77159ee750f50b0a71b564aae6782b81d7119
anand <anandapadmanabhanvt@gmail.com>
2021-11-03 18:13:22 +0530

    initial commit till model

---
+++ server/server/base/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+# class Product(models.Model):
+    # user = 
+    # name = Model.
+    # image = 
+    # description = 
+    # brand =
+    # category =
+    # price =
+    # countInStock =
+    # rating = 
+    # numReviews =
